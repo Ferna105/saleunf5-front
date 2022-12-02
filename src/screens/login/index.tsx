ebba3db881b03b46180f978fcbea000ad89d544d
Fernando Mariscotti <fernandomariscotti@MBP-de-Fernando.fibertel.com.ar>
2022-12-02 03:08:05 -0300

    Integración login google funcionando

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import {Container, Text} from 'components';
-import {View} from 'react-native';
+import {Button, View} from 'react-native';
 import {styles} from './styles';
 
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
 export const Login = () => {
+  GoogleSignin.configure({
+    webClientId:
+      '325980594852-ar53h789aia7pubkrmjr4td26m6b28c2.apps.googleusercontent.com',
+  });
+
+  const onPressGoogleBtn = async () => {
+    const {idToken} = await GoogleSignin.signIn();
+    console.log({idToken});
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    auth()
+      .signInWithCredential(googleCredential)
+      .then(user => {
+        console.log({user});
+      })
+      .catch(error => console.log(error));
+  };
+
   return (
     <Container style={styles.container}>
       <Text>Iniciar sesión</Text>
@@ -13,6 +33,7 @@ export const Login = () => {
       <View>
         <Text>Contraseña</Text>
       </View>
+      <Button onPress={onPressGoogleBtn} title="Iniciar sesión con Google" />
     </Container>
   );
 };
